8ceb5547dbc48111dbf15f919d169ed6c0e3dfbc
jonesj5901 <jailynjones591@gmail.com>
2023-11-30 20:53:44 -0500

    .

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,15 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+from pathlib import Path
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import Engine, create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
@@ -8,12 +18,14 @@
 
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(Engine, reflect = True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -21,7 +33,7 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
